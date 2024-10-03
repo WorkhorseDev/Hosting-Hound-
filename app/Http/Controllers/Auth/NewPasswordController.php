5f5adcb104a295eaae7c83cd9a173b0214df66d0
tatyanakoba <workkoba@gmail.com>
2024-10-03 15:37:27 +0300

    validate password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -37,7 +37,7 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => 'required',
             //'email' => 'required|email',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', 'current_password' => ['required','current_password'], Rules\Password::defaults()],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
