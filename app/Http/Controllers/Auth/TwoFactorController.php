aba5b2b1dac02cd68db54c776483dfedf6136a8f
tatyanakoba <workkoba@gmail.com>
2024-10-08 13:31:14 +0300

    2 factor

--- app/Http/Controllers/Auth/TwoFactorController.php
+++ app/Http/Controllers/Auth/TwoFactorController.php
@@ -32,7 +32,9 @@ class TwoFactorController extends Controller
         {
             $user->resetTwoFactorCode();
 
-            return redirect('/');
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('dashboard', absolute: false));
         }
 
         return redirect()->back()->withErrors(['two_factor_code' => 'The two factor code you have entered does not match']);
