ebd22be3c08fbad26c4d1d178d0100d4919e64b6
tatyanakoba <workkoba@gmail.com>
2024-10-08 13:01:46 +0300

    2 factor

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -20,7 +20,7 @@ class WebsiteController extends Controller
      */
     public function __construct()
     {
-     //   $this->middleware('auth')->except(['setLocale', 'showPrivacyPolicy']);
+
     }
 
     /**
