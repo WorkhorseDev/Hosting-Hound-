bf9efbe463c649a6a5ecf98104427d7a1c2d3f00
tatyanakoba <workkoba@gmail.com>
2024-10-11 12:29:12 +0300

    reset password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -38,11 +38,9 @@ class NewPasswordController extends Controller
         $link  = $_SERVER['HTTP_REFERER'];
         $arrayEmail = explode('email=',$_SERVER['HTTP_REFERER']);
         $email  = urldecode($arrayEmail[1]);
-//        $request->validate([
-//            'token' => 'required',
-//            //'email' => 'required|email',
-//            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-//        ]);
+        $request->validate([
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
         $user = User::where('email',$email)->first();
         $request->validate([
             'token' => 'required',
@@ -53,33 +51,9 @@ class NewPasswordController extends Controller
                 }
             }],
         ]);
-        $request->email = $email;
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            //$request->only('password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'email' => $request->email,
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
-                ])->save();
+       $user->password = Hash::make($request->password);
 
-                event(new PasswordReset($user));
-            }
-        );
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
-        //if ($status == Password::PASSWORD_RESET) {
-            return redirect()->route('login');
-        //}
-
-//        throw ValidationException::withMessages([
-//            'email' => [trans($status)],
-//        ]);
+        return redirect()->route('login');
     }
 }
