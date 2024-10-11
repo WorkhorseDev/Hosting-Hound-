354b35e8cdc06d1c0558c02df92c87015eba3fdf
tatyanakoba <workkoba@gmail.com>
2024-10-11 15:12:41 +0300

    reset password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
@@ -18,12 +15,6 @@ class PasswordResetLinkController extends Controller
     /**
      * Display the password reset link request view.
      */
-    /**
-     * The callback that should be used to create the reset password URL.
-     *
-     * @var (\Closure(mixed, string): string)|null
-     */
-    public static $createUrlCallback;
     public function create(): Response
     {
         return Inertia::render('Auth/ForgotPassword', [
@@ -45,12 +36,9 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $url =$this->resetUrl( $request->only('email'));
-        $status = $this->buildMailMessage($url);
-//        $status =
-//            Password::sendResetLink(
-//            $request->only('email')
-//        );
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
 
         if ($status == Password::RESET_LINK_SENT) {
             return back()->with('status', __($status));
@@ -59,34 +47,5 @@ class PasswordResetLinkController extends Controller
         throw ValidationException::withMessages([
             'email' => [trans($status)],
         ]);
-    }
-
-    protected function resetUrl($notifiable)
-    {
-        if (static::$createUrlCallback) {
-            return call_user_func(static::$createUrlCallback, $notifiable, $this->token);
-        }
-
-        return url(route('password.reset', [
-            'token' => Password::createToken(),
-            'email' => $notifiable->getEmailForPasswordReset(),
-        ], false));
-    }
-
-    /**
-     * Get the reset password notification mail message for the given URL.
-     *
-     * @param  string  $url
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    protected function buildMailMessage($url)
-    {
-
-        return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 }
