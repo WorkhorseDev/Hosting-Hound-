56b262f32b193c627db9e9c93fd771eef9e1c6e2
tatyanakoba <workkoba@gmail.com>
2024-10-24 14:48:14 +0300

    style for new site

--- resources/js/Pages/AddSite.vue
+++ resources/js/Pages/AddSite.vue
@@ -112,11 +112,7 @@ const path = window.location.pathname;
                                 <div class="mb-5 right color">
                                     <label for="color" class="block text-sm font-medium leading-6 text-gray-900">Color</label>
                                     <div class="mt-2">
-                                        <select v-model="form.color" id="color" name="color" autocomplete="color" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                            <option>Red</option>
-                                            <option>Blue</option>
-                                            <option>Green</option>
-                                        </select>
+                                      <input type="color" v-model="form.color" id="color" name="color" value="#ff920a">
                                     </div>
                                 </div>
 
