8b7d61d8bc4c966eec34fc5cde5da2840ef0479d
oleksiiloish <aleksey.loish@gmail.com>
2024-11-12 09:34:47 +0200

    styled service provider form (draft)

--- resources/js/Pages/AddSite.vue
+++ resources/js/Pages/AddSite.vue
@@ -314,6 +314,9 @@ export default {
     background-color: #ffffff;
     color: #7a8088;
 }
+.form-group select {
+    background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>');
+}
 
 .form-group input.bg-grey,
 .form-group select.bg-grey {
@@ -363,25 +366,20 @@ export default {
 
 .form-footer {
     display: flex;
-    gap: 10px;
+    gap: 15px;
 }
 
 .form-footer button {
     flex: 1;
     padding: 10px;
-    font-size: 16px;
+    font-size: 1.2rem;
     font-weight: bold;
     border: none;
-    border-radius: 8px;
+    border-radius: 30px;
     cursor: pointer;
-}
-
-.cancel-btn {
-    background-color: #3a5656;
-    color: white;
-}
-
-.submit-btn {
+    webkit-box-shadow: 0px 5px 8px -4px rgba(0,0,0,0.5);
+    -moz-box-shadow: 0px 5px 8px -4px rgba(0,0,0,0.5);
+    box-shadow: 0px 5px 8px -4px rgba(0,0,0,0.5);
     background-color: #3a5656;
     color: white;
 }
